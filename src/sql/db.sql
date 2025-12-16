cdbe3ae3edbe9ce7d3fb2a25c56cd488f526d7e7
Yoan <yoandiny14@gmail.com>
2025-12-16 14:56:13 +0300

    rearranged user creation request

--- src/sql/db.sql
+++ src/sql/db.sql
@@ -1,5 +1,5 @@
 create database mini_dish_db;
-create user mini_dish_db_manager with password null;
+create user mini_dish_db_manager with password 123456;
 grant connect on database mini_dish_db to mini_dish_db_manager;
 grant update, delete, insert, select on all tables in schema public to mini_dish_db_manager;
 grant create on schema public to mini_dish_db_manager;
