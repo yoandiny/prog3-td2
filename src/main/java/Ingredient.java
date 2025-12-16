1b48c7c93ba38711c22909b534041e1a84cb6029
Yoan <yoandiny14@gmail.com>
2025-12-16 14:10:36 +0300

    created Ingredient class and CategoryENum

--- src/main/java/Ingredient.java
+++ src/main/java/Ingredient.java
@@ -1,2 +1,11 @@
 public class Ingredient {
+    private int id;
+    private String name;
+    private double price;
+    private CategoryEnum category;
+    private Dish dish;
+
+    public String getDishName(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
