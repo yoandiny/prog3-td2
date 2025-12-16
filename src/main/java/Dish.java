553fee48a6849b34d4c233f888a0f1fb74a26fc8
Yoan <yoandiny14@gmail.com>
2025-12-16 14:06:46 +0300

    created Dish class and DishType Enum

---
+++ src/main/java/Dish.java
@@ -0,0 +1,11 @@
+public class Dish {
+    private int id;
+    private String name;
+    private DishTypeEnum dishType;
+    private List<Ingredient> ingredients;
+
+    public Double getDishPrice() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
